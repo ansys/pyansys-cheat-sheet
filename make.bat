85ca2a153d68278667e22bbe4e2f8c3e0897da61
Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>
2023-06-14 10:11:17 +0200

    fix: errors on build (#33)
    * fix: remove make all target
    
    * fix: solving make all on Windows
    
    * Update static/style.sty
    
    * FIX: Add help for no command
    
    ---------
    
    Co-authored-by: Revathyvenugopal162 <revathy.venugopal@ansys.com>

--- make.bat
+++ make.bat
@@ -3,13 +3,8 @@
 pushd %~dp0
 
 SET BUILDDIR=_build
-SET CHEATSHEETS=pymapdl_cheat_sheet pyfluent_cheat_sheet pyaedt_API_cheat_sheet pyedb_API_cheat_sheet
 
-rem Note: If you are using make.bat file for building locally, change L2 of .tex files from 
-rem ``\usepackage{{./../static/style}}`` to ``\usepackage{{.\..\static\style}}``
-
-all: %CHEATSHEETS% 
-
+if "%1" == "all" goto all
 if "%1" == "pymapdl_cheat_sheet" goto pymapdl_cheat_sheet
 if "%1" == "pyfluent_cheat_sheet" goto pyfluent_cheat_sheet
 if "%1" == "pyaedt_API_cheat_sheet" goto pyaedt_API_cheat_sheet
@@ -17,25 +12,34 @@ if "%1" == "pyedb_API_cheat_sheet" goto pyedb_API_cheat_sheet
 if "%1" == "pyprimemesh_cheat_sheet" goto pyedb_API_cheat_sheet
 if "%1" == "clean" goto clean
 if "%1" == "help" goto help
+if "%1" == "" goto help
 
 :pymapdl_cheat_sheet
-    pdflatex -include-directory="cheat_sheets\mapdl_cheat_sheet" -output-directory=%BUILDDIR% pymapdl_cheat_sheet.tex --interaction=nonstopmode
+    pdflatex -output-directory=%BUILDDIR% cheat_sheets/mapdl_cheat_sheet/pymapdl_cheat_sheet.tex --interaction=nonstopmode
     goto end
 
 :pyfluent_cheat_sheet
-    pdflatex -include-directory="cheat_sheets\pyfluent_cheat_sheet" -output-directory=%BUILDDIR% pyfluent_cheat_sheet.tex --interaction=nonstopmode
+    pdflatex -output-directory=%BUILDDIR% cheat_sheets/pyfluent_cheat_sheet/pyfluent_cheat_sheet.tex --interaction=nonstopmode
     goto end
 
 :pyaedt_API_cheat_sheet
-    pdflatex -include-directory="cheat_sheets\aedt_cheat_sheet" -output-directory=%BUILDDIR% pyaedt_API_cheat_sheet.tex --interaction=nonstopmode
+    pdflatex -output-directory=%BUILDDIR% cheat_sheets/aedt_cheat_sheet/pyaedt_API_cheat_sheet.tex --interaction=nonstopmode
     goto end
 
 :pyedb_API_cheat_sheet
-    pdflatex -include-directory="cheat_sheets\aedt_cheat_sheet" -output-directory=%BUILDDIR% pyedb_API_cheat_sheet.tex --interaction=nonstopmode
+    pdflatex -output-directory=%BUILDDIR% cheat_sheets/aedt_cheat_sheet/pyedb_API_cheat_sheet.tex --interaction=nonstopmode
     goto end
 
 :pyprimemesh_cheat_sheet
-    pdflatex -include-directory="cheat_sheets\pyprimesh_cheat_sheet" -output-directory=%BUILDDIR% pyprimemesh_cheat_sheet.tex --interaction=nonstopmode
+    pdflatex -output-directory=%BUILDDIR% cheat_sheets/pyprimemesh_cheat_sheet/pyprimemesh_cheat_sheet.tex --interaction=nonstopmode
+    goto end
+
+:all
+    pdflatex -output-directory=%BUILDDIR% cheat_sheets/mapdl_cheat_sheet/pymapdl_cheat_sheet.tex --interaction=nonstopmode
+    pdflatex -output-directory=%BUILDDIR% cheat_sheets/pyfluent_cheat_sheet/pyfluent_cheat_sheet.tex --interaction=nonstopmode
+    pdflatex -output-directory=%BUILDDIR% cheat_sheets/aedt_cheat_sheet/pyaedt_API_cheat_sheet.tex --interaction=nonstopmode
+    pdflatex -output-directory=%BUILDDIR% cheat_sheets/aedt_cheat_sheet/pyedb_API_cheat_sheet.tex --interaction=nonstopmode
+    pdflatex -output-directory=%BUILDDIR% cheat_sheets/pyprimemesh_cheat_sheet/pyprimemesh_cheat_sheet.tex --interaction=nonstopmode
     goto end
 
 :clean
