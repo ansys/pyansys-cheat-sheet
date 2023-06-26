24d0c1071194130f209cbefbf66e81bbf47280bf
Revathyvenugopal162 <revathy.venugopal@ansys.com>
2023-06-26 09:42:05 +0200

    Doc: update the output file

--- scripts/generate_code_snippet.py
+++ scripts/generate_code_snippet.py
@@ -1,15 +1,17 @@
 """Provides functions for generating code examples from a Python file."""
 import os
+import argparse
 
-def generate_code_examples(file_path, output_dir):
+THIS_PATH = os.path.dirname(os.path.abspath(__file__))
+OUTPUT_PATH = os.path.join(THIS_PATH, "generated_scripts")
+
+def generate_code_examples(file_path):
     """Generate code examples from a Python file.
 
     Parameters
     ----------
     file_path : str
         Path to the Python file.
-    output_dir : str
-        Path to the output directory.
 
     """
     with open(file_path, "r") as file:
@@ -21,7 +23,7 @@ def generate_code_examples(file_path, output_dir):
 
     for i, example in enumerate(examples):
         file_name = f"{script_name}_{i+1}.py"
-        file_path = os.path.join(output_dir, file_name)
+        file_path = os.path.join(OUTPUT_PATH, file_name)
         with open(file_path, "w") as file:
             file.write(example.strip())
 
