36de4c71bf7f8081bcab05711a2df35012429d0f
Revathyvenugopal162 <revathy.venugopal@ansys.com>
2023-07-21 11:54:29 +0200

    Fix: add the blkack formatting

--- cheat_sheets/pymechanical_cheat_sheet/pymechanical_script.py
+++ cheat_sheets/pymechanical_cheat_sheet/pymechanical_script.py
@@ -7,10 +7,10 @@ mechanical = pymechanical.launch_mechanical()
 # BREAK BLOCK
 import ansys.mechanical.core as pymechanical
 
-#Local . default port is 10000
-mechanical = pymechanical.Mechanical(port= 10000)
+# Local . default port is 10000
+mechanical = pymechanical.Mechanical(port=10000)
 
-#Remote . IP address or hostname and port.
+# Remote . IP address or hostname and port.
 mechanical = pymechanical.Mechanical("192.168.0.1", port=10000)
 
 # BREAK BLOCK
@@ -18,20 +18,26 @@ print(mechanical)
 
 # BREAK BLOCK
 from ansys.mechanical.core import find_mechanical
+
 wb_exe = find_mechanical(232)[0]
 # 'Ansys Inc\\v232\\aisol\\bin\\winx64\\AnsysWBU.exe'
-mechanical = launch_mechanical(exec_file=wb_exe, verbose_mechanical=True, batch=True)
+mechanical = launch_mechanical(
+    exec_file=wb_exe, verbose_mechanical=True, batch=True
+)
 print(mechanical)
 # BREAK BLOCK
 mechanical = pymechanical.launch_mechanical(batch=False)
 # BREAK BLOCK
 result1 = mechanical.run_python_script("2+3")
-result2 = mechanical.run_python_script("ExtAPI.DataModel.Project.ProjectDirectory")
-mechanical.run_python_script("Model.AddStaticStructuralAnalysis()")
+result2 = mechanical.run_python_script(
+    "ExtAPI.DataModel.Project.ProjectDirectory"
+)
+mechanical.run_python_script(
+    "Model.AddStaticStructuralAnalysis()"
+)
 # BREAK BLOCK
-
 # To Import a  Material
-commands="""
+commands = """
 cu_mat__file_path = r'D:\Workdir\copper.xml'.replace("\\", "\\\\")
 materials = ExtAPI.DataModel.Project.Model.Materials
 materials.Import(cu_mat__file_path)"""
@@ -39,66 +45,61 @@ mechanical.run_python_script(commands)
 # BREAK BLOCK
 mechanical.run_python_script_from_file(file_path)
 # BREAK BLOCK
-
-
-
-file =  r'D:\\Workdir\\bracket.mechdb'
+file = r"D:\\Workdir\\bracket.mechdb"
 command = f'ExtAPI.DataModel.Project.Open("{file}")'
 mechanical.run_python_script(command)
-
-mechanical.run_python_script("allbodies=ExtAPI.DataModel.Project.Model.GetChildren( DataModelObjectCategory.Body,True)")
+mechanical.run_python_script(
+    "allbodies=ExtAPI.DataModel.Project.Model.GetChildren( DataModelObjectCategory.Body,True)"
+)
 mechanical.run_python_script("allbodies.Count")
-
 # BREAK BLOCK
-
-
-#Get the project directory
+# Get the project directory
 mechanical.project_directory
-
-#List the files in the working directory.
+# List the files in the working directory.
 mechanical.list_files()
-
-#Save
-mechanical.run_python_script("ExtAPI.DataModel.Project.Save(r'D:\\Workdir')")
-
-#Logging in two ways:
+# Save
+mechanical.run_python_script(
+    "ExtAPI.DataModel.Project.Save(r'D:\\Workdir')"
+)
+# Logging in two ways:
 mechanical._log.info("This is an useful message")
-mechanical.log_message('INFO', 'info message')
-
-#Exit 
+mechanical.log_message("INFO", "info message")
+# Exit
 mechanical.exit(force=True)
 
 
 # BREAK BLOCK
 from ansys.mechanical.core import App
-app = App(version =232)
+
+app = App(version=232)
 print(app)
-
-
 # BREAK BLOCK
-from ansys.mechanical.core global_variables
+from ansys.mechanical.core import global_variables
+
 globals().update(global_variables(app))
+# BREAK BLOCK
+ExtAPI  # Application.ExtAPI
+DataModel  # Application.DataModel
+Model  # Application.DataModel.Project.Model
+Tree  # Application.DataModel.Tree
+Graphics  # Application.ExtAPI.Graphics
 
 # BREAK BLOCK
-ExtAPI      # Application.ExtAPI
-DataModel   # Application.DataModel
-Model       # Application.DataModel.Project.Model
-Tree        # Application.DataModel.Tree
-Graphics    # Application.ExtAPI.Graphics
-
-# BREAK BLOCK
-file =  r'D:\\Workdir\\bracket.mechdb'
+file = r"D:\\Workdir\\bracket.mechdb"
 app.open(file)
-
-allbodies = DataModel.Project.Model.GetChildren( Ansys.Mechanical.DataModel.Enums.DataModelObjectCategory.Body,True)
+allbodies = DataModel.Project.Model.GetChildren(
+    Ansys.Mechanical.DataModel.Enums.DataModelObjectCategory.Body,
+    True,
+)
 print(allbodies.Count)
-
-
 # BREAK BLOCK
 import logging
-from ansys.mechanical.core import App 
-from ansys.mechanical.core.embedding.logger import Configuration, Logger
+from ansys.mechanical.core import App
+from ansys.mechanical.core.embedding.logger import (
+    Configuration,
+    Logger,
+)
 
 Configuration.configure(level=logging.WARNING, to_stdout=True)
-app = App(version= 232)
+app = App(version=232)
 Logger.error("message")
